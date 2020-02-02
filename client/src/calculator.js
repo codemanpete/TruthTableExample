8b47bafd901447e6c2d749e9ee2441acb663c7af
codemanpete <rhoxwarrior@gmail.com>
2020-02-01 19:29:54 -0600

    Basic Truth Table generator implemented (No error checking, Needs formatting)

--- client/src/calculator.js
+++ client/src/calculator.js
@@ -5,13 +5,42 @@ import Statement from './engine/statement.js';
 
 
 class Calculator extends React.Component {
-  render() {
-    let statement = new Statement('(P <-> Q) <-> ((P || R) -> (~Q -> R))');
+  constructor(props) {
+    super(props);
+    window.Statement = Statement;
+    this.state = {
+      wff: "",
+      out: ""
+    };
+    this.updateWff = this.updateWff.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  updateWff(e) {
+    this.setState({ wff: e.currentTarget.value });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    let statement = new Statement(this.state.wff);
+    console.log(statement.table());
+    this.setState({ out: statement.table().replace(/&#124;/g, '|') });
+  }
+
+  render() {
+    let output = this.state.out.split('\n').map((item, i) => {
+      return <p key={i}>{item}</p>;
+    });
     return (
       <div>
-        {statement.table()}
-      </div >
+        <h3>Example</h3>
+        <p>Type a wff in the box</p>
+        <textarea value={this.state.wff} onChange={this.updateWff}></textarea>
+        <br />
+        <button onClick={this.handleClick}>Submit</button>
+        <br />
+        {output}
+      </div>
     );
   }
 }
